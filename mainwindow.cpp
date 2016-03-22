b455d25dc368d0b7827a63c058a96d8a5b81aee8
Dominik Koszkul <dominik.koszkul@gmail.com>
2016-03-22 20:09:15 +0100

    add console view

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 
 #include "paintwidget.h"
+#include "console/settingsdialog.h"
 
 #include <iostream>
 
@@ -10,6 +11,23 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    console = new Console;
+    console->setEnabled(false);
+   // console->setMaximumHeight(100);
+    ui->consoleLayout->addWidget(console);
+
+    serial = new QSerialPort(this);
+    status = new QLabel;
+    settings = new SettingsDialog;
+
+    /*ui->actionConnect->setEnabled(true);
+    ui->actionDisconnect->setEnabled(false);
+    ui->actionQuit->setEnabled(true);
+    ui->actionConfigure->setEnabled(true);*/
+
+    //ui->statusBar->addWidget(status);
+
+
     this->simulation = new Simulation();
 
     list<Obstacle*> obstacles;
@@ -29,9 +47,98 @@ MainWindow::MainWindow(QWidget *parent) :
     simulation->setObstacles(obstacles);
 
     connect(ui->pushButton, &QPushButton::clicked, simulation, &Simulation::simulate);
+
+    connect(serial, SIGNAL(error(QSerialPort::SerialPortError)), this, SLOT(handleError(QSerialPort::SerialPortError)));
+    connect(serial, SIGNAL(readyRead()), this, SLOT(readData()));
+    connect(console, SIGNAL(getData(QByteArray)), this, SLOT(writeData(QByteArray)));
+
+    connect(ui->uartSettingsButton, SIGNAL(clicked()), settings, SLOT(show()));
+    connect(ui->connectButton, SIGNAL(clicked()), this, SLOT(openSerialPort()));
+    connect(ui->disconnectButton, SIGNAL(clicked()), this, SLOT(closeSerialPort()));
+    connect(ui->clearConsoleButton, SIGNAL(clicked()), console, SLOT(clear()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::openSerialPort()
+{
+    SettingsDialog::Settings p = settings->settings();
+    serial->setPortName(p.name);
+    serial->setBaudRate(p.baudRate);
+    serial->setDataBits(p.dataBits);
+    serial->setParity(p.parity);
+    serial->setStopBits(p.stopBits);
+    serial->setFlowControl(p.flowControl);
+    if (serial->open(QIODevice::ReadWrite)) {
+        console->setEnabled(true);
+        console->setLocalEchoEnabled(p.localEchoEnabled);
+        /*ui->actionConnect->setEnabled(false);
+        ui->actionDisconnect->setEnabled(true);
+        ui->actionConfigure->setEnabled(false);*/
+        showStatusMessage(tr("Connected to %1 : %2, %3, %4, %5, %6")
+                          .arg(p.name).arg(p.stringBaudRate).arg(p.stringDataBits)
+                          .arg(p.stringParity).arg(p.stringStopBits).arg(p.stringFlowControl));
+    } else {
+        QMessageBox::critical(this, tr("Error"), serial->errorString());
+
+        showStatusMessage(tr("Open error"));
+    }
+}
+//! [4]
+
+//! [5]
+void MainWindow::closeSerialPort()
+{
+    if (serial->isOpen())
+        serial->close();
+    console->setEnabled(false);
+   /* ui->actionConnect->setEnabled(true);
+    ui->actionDisconnect->setEnabled(false);
+    ui->actionConfigure->setEnabled(true);*/
+    showStatusMessage(tr("Disconnected"));
+}
+
+//! [6]
+void MainWindow::writeData(const QByteArray &data)
+{
+    serial->write(data);
+}
+//! [6]
+
+//! [7]
+void MainWindow::readData()
+{
+    QByteArray data = serial->readAll();
+    console->putData(data);
+    //ui->lcdNumber_3->display((ui->lcdNumber_3->value())+1);
+}
+//! [7]
+
+//! [8]
+void MainWindow::handleError(QSerialPort::SerialPortError error)
+{
+    if (error == QSerialPort::ResourceError) {
+        QMessageBox::critical(this, tr("Critical Error"), serial->errorString());
+        closeSerialPort();
+    }
+}
+//! [8]
+
+void MainWindow::initActionsConnections()
+{
+    /*connect(ui->actionConnect, SIGNAL(triggered()), this, SLOT(openSerialPort()));
+    connect(ui->actionDisconnect, SIGNAL(triggered()), this, SLOT(closeSerialPort()));
+    connect(ui->actionQuit, SIGNAL(triggered()), this, SLOT(close()));
+    connect(ui->actionConfigure, SIGNAL(triggered()), settings, SLOT(show()));
+    connect(ui->actionClear, SIGNAL(triggered()), console, SLOT(clear()));
+    connect(ui->actionAbout, SIGNAL(triggered()), this, SLOT(about()));
+    connect(ui->actionAboutQt, SIGNAL(triggered()), qApp, SLOT(aboutQt()));*/
+}
+
+void MainWindow::showStatusMessage(const QString &message)
+{
+    status->setText(message);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,13 +1,29 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QtCore/QtGlobal>
 #include <QMainWindow>
+#include <QtSerialPort/QSerialPort>
+
+#include <QMessageBox>
+#include <QLabel>
+#include <QtSerialPort/QSerialPort>
+
 #include "simulation.h"
+#include "console/console.h"
+
+QT_BEGIN_NAMESPACE
+
+class QLabel;
+
 namespace Ui {
 class MainWindow;
 }
 
-using namespace std;
+QT_END_NAMESPACE
+
+class Console;
+class SettingsDialog;
 
 class MainWindow : public QMainWindow
 {
@@ -17,9 +33,26 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void openSerialPort();
+    void closeSerialPort();
+    void writeData(const QByteArray &data);
+    void readData();
+
+    void handleError(QSerialPort::SerialPortError error);
+
 private:
     Ui::MainWindow *ui;
     Simulation* simulation;
+
+    void initActionsConnections();
+    void showStatusMessage(const QString &message);
+
+    QLabel *status;
+    Console *console;
+    QSerialPort *serial;
+    SettingsDialog *settings;
+
 };
 
 #endif // MAINWINDOW_H
